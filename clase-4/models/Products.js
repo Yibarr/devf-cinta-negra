f1dd5918b35e12cb612a50658ac3ace3a4671e42
yibarr <yael@os.city>
2020-09-28 19:11:20 -0500

    clase-4

---
+++ clase-4/models/Products.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  stock: {
+    store: {
+      type: String,
+      default: 'La comer'
+    },
+    qnty: {
+      type: Number,
+      default: 25
+    }
+  }
+}, { timestamps: true })
+
+const Products = mongoose.model('Products', productSchema)
+
+module.exports = Products
---
+++ clase-4/mongoClient/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(
+  process.env.MONGO_URI
+  ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+  .then(() => {
+    console.log('Connected to Mongo DB');
+  } )
+  .catch(err => {
+    console.log('Error connecting to database', err.message);
+  })
